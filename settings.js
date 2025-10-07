70c97b34551ad50fc9cfc14bac3036bb03a1fd0b
WEB3GOOSIP <uduakobongusoro8@gmail.com>
2025-10-07 12:59:16 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Web3Goosip', // Your name
-  ownerNumber: '2349032886041', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '234808723569', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
